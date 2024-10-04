968bf65a2facd98ba852f7c4b624cd1ad9f76269
Leandro Peres <leandroperes@protonmail.com>
2024-10-04 07:45:17 -0300

    🍱 Hello World identation

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -9,26 +9,27 @@ const initialState: AppState = {
     {
       id: "main.cpp",
       content: `#include <iostream>
-  #include <string>
-  #include <vector>
-  
-  template<typename T>
-  std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
-  {
-      for (auto& el : vec)
-      {
-          os << el << ' ';
-      }
-      return os;
-  }
-  
-  int main()
-  {
-      std::vector<std::string> vec = {
-          "Hello", "from", "GCC", __VERSION__, "!" 
-      };
-      std::cout << vec << std::endl;
-  }`
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> vec = {
+        "Hello", "from", "GCC", __VERSION__, "!" 
+    };
+    std::cout << vec << std::endl;
+}
+`
     }
   ],
   activeTab: "main.cpp",
